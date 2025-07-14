05bc07d5eeaec78c9fdb830a5e39bf6e3034b439
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-07-14 16:09:37 +0100

    Update project.controller.js

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -25,7 +25,7 @@ module.exports = {
         description,
         startDate,
         endDate,
-        status: "pending",
+        status: "To Do",
       });
       return res
         .status(201)
