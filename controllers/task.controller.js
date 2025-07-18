456fc563b2ebbfb6b28530561777cb8fd87934ad
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-07-18 13:34:58 +0100

    Update task.controller.js

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-const { sendMail } = require("../utils/mailer");
+const sendMail = require("../utils/mailer");
 
 exports.createTask = async (req, res) => {
   try {
