e9f2c7ee403c7f83c35ebf43de16324c801bd017
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-07-25 10:55:32 +0100

    Update team.model.js

--- models/team.model.js
+++ models/team.model.js
@@ -1,3 +1,4 @@
+// models/team.model.js
 module.exports = (sequelize, DataTypes) => {
   const Team = sequelize.define("Team", {
     id: {
@@ -14,6 +15,23 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: true,
     },
   });
+
+  //Team.associate = (models) => {
+    //Team.belongsToMany(models.User, {
+     // through: models.UserTeam,
+      //foreignKey: "teamId",
+     // otherKey: "userId",
+   // });
+   // Team.belongsToMany(models.Project, {
+    //  through: models.TeamProject,
+     // foreignKey: "teamId",
+     // otherKey: "projectId",
+    //});
+    //Team.hasMany(models.UserTeam, {
+     // foreignKey: "teamId",
+     // onDelete: "CASCADE",
+    //});
+ // };
 
   return Team;
 };
