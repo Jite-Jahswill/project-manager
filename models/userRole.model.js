275e83c1363ee852c8a34a360d4b57a058655aad
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-10-25 18:23:41 +0100

    Create userRole.model.js

---
+++ models/userRole.model.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+  const UserRole = sequelize.define("UserRole", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+  });
+
+  return UserRole;
+};
