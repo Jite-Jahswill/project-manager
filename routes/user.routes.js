5f9917f87fcefe7d253c3ca97d79ff5e6c85f743
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-10-26 20:31:10 +0100

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const userController = require("../controllers/user.controller");
+const userController = require("../controllers/userController");
 const { verifyToken } = require("../middlewares/auth.middleware");
 
 module.exports = (app) => {
