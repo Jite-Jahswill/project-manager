4cc1869b6af47b482bfc2cfb4fadc90666bc0520
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-03 11:00:48 +0100

    Update participant.model.js

--- models/participant.model.js
+++ models/participant.model.js
@@ -5,20 +5,6 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
       autoIncrement: true,
     },
-    conversationId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "Conversations",
-        key: "id",
-      },
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "Users",
-        key: "id",
-      },
-    },
     joinedAt: {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW,
