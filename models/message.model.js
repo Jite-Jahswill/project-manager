08acc41962e3124d9513a5b338a5996b56205495
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-04 19:39:31 +0100

    Update message.model.js

--- models/message.model.js
+++ models/message.model.js
@@ -1,27 +1,35 @@
 module.exports = (sequelize, DataTypes) => {
   const Message = sequelize.define("Message", {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    conversationId: { type: DataTypes.INTEGER, allowNull: false },
-    senderId: { type: DataTypes.INTEGER, allowNull: false },
-    content: { type: DataTypes.TEXT, allowNull: false },
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    conversationId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    senderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
     type: {
-      type: DataTypes.ENUM("text", "image", "file", "system"),
+      type: DataTypes.ENUM("text", "image", "file"),
       defaultValue: "text",
     },
-    isEdited: { type: DataTypes.BOOLEAN, defaultValue: false },
-    isDeleted: { type: DataTypes.BOOLEAN, defaultValue: false },
+    isEdited: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    isDeleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   });
-
-  Message.associate = (models) => {
-    Message.belongsTo(models.Conversation, {
-      as: "conversation",
-      foreignKey: "conversationId",
-    });
-    Message.belongsTo(models.User, {
-      as: "sender",
-      foreignKey: "senderId",
-    });
-  };
 
   return Message;
 };
