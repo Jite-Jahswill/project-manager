b848da25778e248d3b722bc155818822142a8ea5
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-04 18:49:22 +0100

    Create messaging.routes.js

---
+++ routes/messaging.routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const verifyToken = require("../middlewares/auth.middleware");
+const messaging = require("../controllers/messaging.controller");
+
+// Direct chat
+router.post("/direct/:recipientId", verifyToken, messaging.startDirectChat);
+
+// Messages CRUD
+router.post("/:conversationId/message", verifyToken, messaging.sendMessage);
+router.get("/:conversationId/messages", verifyToken, messaging.getAllMessages);
+router.put("/message/:messageId", verifyToken, messaging.editMessage);
+router.delete("/message/:messageId", verifyToken, messaging.deleteMessage);
+
+// Group
+router.post("/group/:conversationId/participant", verifyToken, messaging.addGroupMember);
+router.delete("/conversation/:conversationId", verifyToken, messaging.leaveConversation);
+
+module.exports = router;
