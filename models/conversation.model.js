be39b339c332b1228c159789304eea4e5080bbd5
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-04 19:38:14 +0100

    Update conversation.model.js

--- models/conversation.model.js
+++ models/conversation.model.js
@@ -1,32 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
   const Conversation = sequelize.define("Conversation", {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING, allowNull: true }, // for groups
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     type: {
       type: DataTypes.ENUM("direct", "group"),
       defaultValue: "direct",
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
     createdBy: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
     },
   });
-
-  Conversation.associate = (models) => {
-    Conversation.belongsTo(models.User, {
-      foreignKey: "createdBy",
-      as: "creator",
-    });
-    Conversation.belongsToMany(models.User, {
-      through: models.Participant,
-      as: "participants",
-      foreignKey: "conversationId",
-    });
-    Conversation.hasMany(models.Message, {
-      as: "messages",
-      foreignKey: "conversationId",
-    });
-  };
 
   return Conversation;
 };
