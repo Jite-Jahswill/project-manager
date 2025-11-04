5d9f1ebc92bcb687f689ccde014e35a60b394694
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-04 17:32:38 +0100

    Update report.model.js

--- models/report.model.js
+++ models/report.model.js
@@ -1,37 +1,61 @@
+// models/report.model.js
 module.exports = (sequelize, DataTypes) => {
-  const Report = sequelize.define("Report", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+  const Report = sequelize.define(
+    "Report",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      dateOfReport: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      timeOfReport: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      reporterId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      report: {
+        type: DataTypes.TEXT, // long text
+        allowNull: false,
+      },
+      supportingDocUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      status: {
+        type: DataTypes.ENUM("open", "pending", "closed"),
+        allowNull: false,
+        defaultValue: "open",
+      },
+      closedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      closedBy: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: { model: "Users", key: "id" },
+      },
     },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    projectId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    teamId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: true,
+      tableName: "Reports",
+    }
+  );
 
-  // Report.associate = (models) => {
-  //   Report.belongsTo(models.User, { foreignKey: "userId" });
-  //   Report.belongsTo(models.Project, { foreignKey: "projectId" });
-  //   Report.belongsTo(models.Team, { foreignKey: "teamId" });
-  // };
+  Report.associate = (models) => {
+    Report.belongsTo(models.User, { foreignKey: "reporterId", as: "reporter" });
+    Report.belongsTo(models.User, { foreignKey: "closedBy", as: "closer" });
+    // Keep project/team if you still need them
+    Report.belongsTo(models.Project, { foreignKey: "projectId", as: "project" });
+    Report.belongsTo(models.Team, { foreignKey: "teamId", as: "team" });
+  };
 
   return Report;
 };
