53665041f9640cae0769b020de1b055effd292d2
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-04 19:38:51 +0100

    Update participant.model.js

--- models/participant.model.js
+++ models/participant.model.js
@@ -1,25 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
   const Participant = sequelize.define("Participant", {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    conversationId: { type: DataTypes.INTEGER, allowNull: false },
-    userId: { type: DataTypes.INTEGER, allowNull: false },
-    role: {
-      type: DataTypes.ENUM("member", "admin"),
-      defaultValue: "member",
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    joinedAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
+    conversationId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
   });
-
-  Participant.associate = (models) => {
-    Participant.belongsTo(models.User, { as: "user", foreignKey: "userId" });
-    Participant.belongsTo(models.Conversation, {
-      as: "conversation",
-      foreignKey: "conversationId",
-    });
-  };
 
   return Participant;
 };
