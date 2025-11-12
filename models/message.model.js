ddfde5cb043250558097b57797499a90fb9df0f5
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-12 09:29:10 +0100

    Update message.model.js

--- models/message.model.js
+++ models/message.model.js
@@ -1,43 +1,15 @@
 // models/message.model.js
 module.exports = (sequelize, DataTypes) => {
   const Message = sequelize.define("Message", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    conversationId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    senderId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    receiverId: {                     // NEW
-      type: DataTypes.INTEGER,
-      allowNull: true,                // null for group chats
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    type: {
-      type: DataTypes.ENUM("text", "image", "file"),
-      defaultValue: "text",
-    },
-    isEdited: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isDeleted: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    isRead: {                         // NEW
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    conversationId: { type: DataTypes.INTEGER, allowNull: false },
+    senderId: { type: DataTypes.INTEGER, allowNull: false },
+    receiverId: { type: DataTypes.INTEGER, allowNull: true }, // null = group
+    content: { type: DataTypes.TEXT, allowNull: true },
+    type: { type: DataTypes.ENUM("text", "image", "file"), defaultValue: "text" },
+    isEdited: { type: DataTypes.BOOLEAN, defaultValue: false },
+    isDeleted: { type: DataTypes.BOOLEAN, defaultValue: false },
+    isRead: { type: DataTypes.BOOLEAN, defaultValue: false },
   }, {
     indexes: [
       { fields: ["conversationId"] },
@@ -48,9 +20,9 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   Message.associate = (models) => {
-    // Message.belongsTo(models.User, { foreignKey: "senderId", as: "sender" });
+    Message.belongsTo(models.User, { foreignKey: "senderId", as: "sender" });
     Message.belongsTo(models.User, { foreignKey: "receiverId", as: "receiver" });
-    // Message.belongsTo(models.Conversation, { foreignKey: "conversationId" });
+    Message.belongsTo(models.Conversation, { foreignKey: "conversationId", as: "conversation" });
   };
 
   return Message;
