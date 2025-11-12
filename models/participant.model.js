7ed9d115b14910e5895243a66388cd84a6ce666f
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-12 09:31:38 +0100

    Update participant.model.js

--- models/participant.model.js
+++ models/participant.model.js
@@ -1,12 +1,11 @@
+// models/participant.model.js
 module.exports = (sequelize, DataTypes) => {
   const Participant = sequelize.define("Participant", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    conversationId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    conversationId: { type: DataTypes.INTEGER, allowNull: false },
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+  }, {
+    indexes: [{ unique: true, fields: ["conversationId", "userId"] }],
   });
 
   return Participant;
