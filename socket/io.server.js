9dbc7747cb4e3886524b5239c3444fdea779f231
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-12 09:41:01 +0100

    Update io.server.js

--- socket/io.server.js
+++ socket/io.server.js
@@ -8,7 +8,7 @@ let io;
 const initSocket = (httpServer) => {
   io = new Server(httpServer, {
     cors: {
-      origin: process.env.CLIENT_URL || "http://localhost:3000",
+      origin: process.env.FRONTEND_URL || "http://localhost:3000",
       methods: ["GET", "POST"],
       credentials: true,
     },
