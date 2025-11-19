698fa3a09b1c2d6ec511119a44f00d89a010446b
Jite_Jahswill <101391845+Jite-Jahswill@users.noreply.github.com>
2025-11-19 11:33:23 +0100

    Delete models/auditAssignment.model.js

--- models/auditAssignment.model.js
+++
@@ -1,4 +0,0 @@
-module.exports = (sequelize, DataTypes) => in {
-  const AuditAssignment = sequelize.define("AuditAssignment", {}, { tableName: "AuditAssignments", timestamps: false });
-  return AuditAssignment;
-};
